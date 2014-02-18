2accb80d566500265fd5711650f2835931bb80f0
Max Kleucker <max.kleucker@gmail.com>
2014-02-18 18:31:22 +0100

    Init config & route to get all POIs

---
+++ LayAR-project/web-api/htdocs/Slim/config.sample.php
@@ -0,0 +1,8 @@
+<?php
+// PLEASE COPY THIS FILE TO `config.php` AND ENTER
+// THE CORRECT ACCESS INFORMATION TO YOUR MySQL-DATABASE.
+
+    $dbhost = "127.0.0.1";
+    $dbuser = "";
+    $dbpass = "";
+    $dbname = "";
--- LayAR-project/web-api/htdocs/index.php
+++ LayAR-project/web-api/htdocs/index.php
@@ -11,78 +11,33 @@ require 'Slim/Slim.php';
 
 \Slim\Slim::registerAutoloader();
 
-/**
- * Step 2: Instantiate a Slim application
- *
- * This example instantiates a Slim application using
- * its default settings. However, you will usually configure
- * your Slim application now by passing an associative array
- * of setting names and values into the application constructor.
- */
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(aray('debug'=>true));
 
-/**
- * Step 3: Define the Slim application routes
- *
- * Here we define several Slim application routes that respond
- * to appropriate HTTP request methods. In this example, the second
- * argument for `Slim::get`, `Slim::post`, `Slim::put`, `Slim::patch`, and `Slim::delete`
- * is an anonymous function.
- */
+$app->get('/poi', 'getPoiAll');
+$app->get('/poi/:id',  'getPoi');
 
-// GET route
-$app->get(
-    '/',
-    function () {
-        $template = <<<EOT
-<!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8"/>
-            <title>LayAR API</title>
-        </head>
-        <body>
-            <p>This is the API for a LayAR project.</p>  
-        </body>
-    </html>
-EOT;
-        echo $template;
-    }
-);
-
-// POST route
-$app->post(
-    '/post',
-    function () {
-        echo 'This is a POST route';
-    }
-);
-
-// PUT route
-$app->put(
-    '/put',
-    function () {
-        echo 'This is a PUT route';
-    }
-);
-
-// PATCH route
-$app->patch('/patch', function () {
-    echo 'This is a PATCH route';
-});
-
-// DELETE route
-$app->delete(
-    '/delete',
-    function () {
-        echo 'This is a DELETE route';
-    }
-);
-
-/**
- * Step 4: Run the Slim application
- *
- * This method should be called last. This executes the Slim application
- * and returns the HTTP response to the HTTP client.
- */
 $app->run();
+
+function getPoiAll(){
+    $sql = "SELECT * FROM poi ORDER BY id DESC";
+
+    try{
+        $db = getConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $pois = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo '{"pois": '. json_encode($pois).'}';
+    } catch(PDOException $e){
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+
+    }
+
+}
+
+function getConnection() {
+    include('config.php');
+    $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+}
