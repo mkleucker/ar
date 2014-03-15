091ed5ac7ecd7485867f0ed1764846c8f831a7d1
Makarovski <saintmakarovski@163.com>
2014-03-15 14:26:47 +0100

    Markerless AR Initial Commit

---
+++ Examples_SDK/CustomRenderer/gen/com/metaio/example_custom_renderer/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.metaio.example_custom_renderer;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
