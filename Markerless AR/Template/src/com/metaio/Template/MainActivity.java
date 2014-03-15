91af54ae4dc460c4ad4f893e170d275d712378f5
Makarovski <saintmakarovski@163.com>
2014-03-15 14:33:18 +0100

    Changing the name of the exercise

---
+++ Markerless AR/Template/src/com/metaio/Template/MainActivity.java
@@ -0,0 +1,97 @@
+// Copyright 2007-2014 metaio GmbH. All rights reserved.
+package com.metaio.Template;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.metaio.sdk.MetaioDebug;
+import com.metaio.tools.io.AssetsManager;
+
+public class MainActivity extends Activity
+{
+	//TODO: Select the template, i.e. Native Android or AREL
+	public static final boolean NATIVE = false;
+	
+	/**
+	 * Task that will extract all the assets
+	 */
+	private AssetsExtracter mTask;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.main);
+		
+		// Enable metaio SDK debug log messages based on build configuration
+		MetaioDebug.enableLogging(BuildConfig.DEBUG);
+		
+		// extract all the assets
+		mTask = new AssetsExtracter();
+		mTask.execute(0);
+
+	}
+
+	/**
+	 * This task extracts all the assets to an external or internal location
+	 * to make them accessible to metaio SDK
+	 */
+	private class AssetsExtracter extends AsyncTask<Integer, Integer, Boolean>
+	{
+
+		@Override
+		protected void onPreExecute() 
+		{
+		}
+		
+		@Override
+		protected Boolean doInBackground(Integer... params) 
+		{
+			try 
+			{
+				// Extract all assets and overwrite existing files if debug build
+				AssetsManager.extractAllAssets(getApplicationContext(), BuildConfig.DEBUG);
+			} 
+			catch (IOException e) 
+			{
+				MetaioDebug.log(Log.ERROR, "Error extracting assets: "+e.getMessage());
+				MetaioDebug.printStackTrace(Log.ERROR, e);
+				return false;
+			}
+			
+			return true;
+		}
+		
+		@Override
+		protected void onPostExecute(Boolean result) 
+		{
+			if(NATIVE)
+			{
+				// create native template and present it
+				Intent intent = new Intent(getApplicationContext(), Template.class);
+				startActivity(intent);
+			}
+			else
+			{
+				// create AREL template and present it
+				final String arelConfigFilePath = AssetsManager.getAssetPath(getApplicationContext(), "arelConfig.xml");
+				MetaioDebug.log("arelConfig to be passed to intent: "+arelConfigFilePath);
+				Intent intent = new Intent(getApplicationContext(), ARELViewActivity.class);
+				intent.putExtra(getPackageName()+".AREL_SCENE", arelConfigFilePath);
+				startActivity(intent);
+				
+			}
+			
+			finish();
+	    }
+		
+	}
+	
+}
+
